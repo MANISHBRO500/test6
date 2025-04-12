96a02a4256116964fd4e4d87d8c6b0a04c1bd225
MANISHBRO500 <160089365+MANISHBRO500@users.noreply.github.com>
2025-04-12 15:48:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -75,5 +75,6 @@ app.delete('/delete-question/:id', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
